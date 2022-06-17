7ee0732b5c0d563a8aec281c779db32196b25560
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-17 16:22:23 +0200

    test validazione email

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,24 +1,23 @@
-// Mail
+
 // 1) Creara un array con lista email
 // 2) Fare prompt per interagire con l'utente
 // 3) Controllare se l'email inserita nel prompt è presente nel array
-//          Se si stampare "Email presente" altrimenti se non è presense "Email non presente" altrimenti "Errore! Email non valida"
+//          Se si stampare "Email presente" altrimenti se non è presense "Email non presente"
 
+// Esercizio 1 - Mail
 
 const emailList = ['mela@frutta.com', 'pera@frutta.com', 'pesca@frutta.com', 'banana@frutta.com', 'fragola@frutta.com'];
-const emailUtente = prompt('Inserisci la tua email');
-console.log(emailList);
+const UserEmail = prompt('Inserisci la tua email');
+let validEmail = false;
 
 for (let index = 0; index < emailList.length; index++) {
 
-    if (emailList[index] === emailUtente) {
-        console.log(index);
-        console.log(emailList[index]);
+    if (emailList[index] === UserEmail) {
+        validEmail = true;
         console.log('Email presente');
-    } else if (emailList[index] !== emailUtente) {
-        console.log(index);
-        console.log(emailList[index]);
+    } else {
         console.log('Email non presente');
     }
-
 }
+
+console.log(emailList);
