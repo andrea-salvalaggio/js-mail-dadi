795a4d29cab4da7e7379b9dd9b3de127604aec5f
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-17 15:39:31 +0200

    verifico le email

--- mail/js/script.js
+++ mail/js/script.js
@@ -2,10 +2,23 @@
 // 1) Creara un array con lista email
 // 2) Fare prompt per interagire con l'utente
 // 3) Controllare se l'email inserita nel prompt è presente nel array
-//          Se si stampare "Email presente" altrimenti "Email non presente"
+//          Se si stampare "Email presente" altrimenti se non è presense "Email non presente" altrimenti "Errore! Email non valida"
 
 
 const emailList = ['mela@frutta.com', 'pera@frutta.com', 'pesca@frutta.com', 'banana@frutta.com', 'fragola@frutta.com'];
 const emailUtente = prompt('Inserisci la tua email');
 console.log(emailList);
 
+for (let index = 0; index < emailList.length; index++) {
+
+    if (emailList[index] === emailUtente) {
+        console.log(index);
+        console.log(emailList[index]);
+        console.log('Email presente');
+    } else if (emailList[index] !== emailUtente) {
+        console.log(index);
+        console.log(emailList[index]);
+        console.log('Email non presente');
+    }
+
+}
