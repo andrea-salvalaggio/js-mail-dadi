5ba5fed6e57ac01094e9403c622dbf7e989ca3cb
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-17 15:08:25 +0200

    first commit

---
+++ mail/js/script.js
@@ -0,0 +1,11 @@
+// Mail
+// 1) Creara un array con lista email
+// 2) Fare prompt per interagire con l'utente
+// 3) Controllare se l'email inserita nel prompt è presente nel array
+//          Se si stampare "Email presente" altrimenti "Email non presente"
+
+
+const emailList = ['mela@frutta.com', 'pera@frutta.com', 'pesca@frutta.com', 'banana@frutta.com', 'fragola@frutta.com'];
+const emailUtente = prompt('Inserisci la tua email');
+console.log(emailList);
+
